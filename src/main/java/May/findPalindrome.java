376848144cbf401363dfd957df652f0d9aa5bfb4
xieli <2524546110@qq.com>
2020-05-08 20:25:07 +0800

    2020/5/8

--- src/main/java/May/findPalindrome.java
+++ src/main/java/May/findPalindrome.java
@@ -5,8 +5,31 @@ import java.util.ArrayList;
 /**
  * Created by xieli on 2020/5/8.
  */
-public class partition {
+public class findPalindrome {
 
-    
+    public static ArrayList<String> findPalindrome(String s) {
+        ArrayList<String> list = new ArrayList<String>();
+        if (s == null || s.length() == 0)
+            return list;
+        for (int i = 0; i < s.length(); i++) {
+            findPalindromeHelper(s, i, i, list);
+            findPalindromeHelper(s, i, i + 1, list);
+        }
+        return list;
+    }
 
+    private static void findPalindromeHelper(String s, int start, int end, ArrayList<String> list) {
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            String subString = s.substring(start, end + 1);
+            if (!list.contains(subString))
+                list.add(subString);
+            start--;
+            end++;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "a";
+        System.out.println(findPalindrome(s));
+    }
 }
--- src/main/java/May/solveNQueens.java
+++ src/main/java/May/solveNQueens.java
@@ -1,7 +1,64 @@
 package May;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by xieli on 2020/5/8.
  */
 public class solveNQueens {
+    List<List<String>> res;
+
+    public List<List<String>> solveNQueens(int n) {
+        if (n <= 0)
+            return null;
+        res = new LinkedList<List<String>>();
+        char[][] board = new char[n][n];
+        for (char[] chars : board)
+            Arrays.fill(chars, '.');
+        backtrack(board, 0);
+        return res;
+    }
+
+    private void backtrack(char[][] board, int row) {
+        if (row == board.length) {
+            res.add(charToString(board));
+            return;
+        }
+
+        int n = board[row].length;
+        for (int col = 0; col < n; col++) {
+            if (!isVaild(board, row, col))
+                continue;
+            board[row][col] = 'Q';
+            backtrack(board, row + 1);
+            board[row][col] = '.';
+        }
+    }
+
+    private boolean isVaild(char[][] board, int row, int col) {
+        int rows = board.length;
+        for (char[] chars : board) {
+            if (chars[col] == 'Q')
+                return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < rows; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+
+    private List<String> charToString(char[][] board) {
+        List<String> result = new LinkedList<String>();
+        for (char[] chars : board) {
+            result.add(String.valueOf(chars));
+        }
+        return result;
+    }
 }
