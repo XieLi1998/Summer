70082c9db8a3e4d9e28ca36e0111a6e9146139be
xieli <2524546110@qq.com>
2020-05-22 23:22:51 +0800

    2020/5/22

---
+++ src/main/java/May/GetNext.java
@@ -0,0 +1,22 @@
+package May;
+
+/**
+ * Created by xieli on 2020/5/22.
+ */
+public class GetNext {
+
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next = null;
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        return null;
+    }
+}
