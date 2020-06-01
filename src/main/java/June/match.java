84b2c63ef9c9e8a77c670401e148b99b3b1debb8
xieli <2524546110@qq.com>
2020-06-01 22:07:16 +0800

    2020/6/1

--- src/main/java/June/match.java
+++ src/main/java/June/match.java
@@ -22,16 +22,14 @@ public class match {
                     (str[i] == pattern[j] || pattern[j] == '.'))
                 return matchCore(str, i, pattern, j + 2) ||
                         matchCore(str, i + 1, pattern, j);
-            //当前不匹配
+                //当前不匹配
             else
                 return matchCore(str, i, pattern, j + 2);
         }
-
         //下一个不是 *
         if (str.length != i &&
                 (str[i] == pattern[j] || pattern[j] == '.'))
             return matchCore(str, i + 1, pattern, j + 1);
-
         return false;
     }
 
