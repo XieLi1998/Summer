fdf52a629a05f4f02e7d85c58545e24f8ff6fa69
xieli <2524546110@qq.com>
2020-06-19 11:48:29 +0800

    2020/6/19

--- src/main/java/June/findNthDigit.java
+++ src/main/java/June/findNthDigit.java
@@ -6,17 +6,35 @@ package June;
 public class findNthDigit {
 
     public int findNthDigit(int n) {
-//        int digit=1;
-//        long start=1;
-//        long count=9;
-//        while (n>count){
-//            n-=count;
-//            digit+=1;
-//            start*=10;
-//            count=9*start*digit;
-//        }
-//        long num=
-        return 0;
+        int digits = 1;
+        long base = 9;
+        while (n - base * digits > 0) {
+            n -= base * digits;
+            digits += 1;
+            base *= 10;
+        }
+
+        // 获取目标位是当前数字的第几位
+        int idx = n % digits;
+        if (idx == 0)
+            idx = digits;
+
+        long num = 1;
+        for (int i = 1; i < digits; i++) {
+            num *= 10;
+        }
+        num += (idx == digits) ? (n / digits - 1) : (n / digits);
+
+        // 将想要的数字控制在最后一位
+        for (int i = idx; i < digits; i++) {
+            num /= 10;
+        }
+        return (int) num % 10;
+    }
+
+    public static void main(String[] args) {
+        findNthDigit findNthDigit = new findNthDigit();
+        System.out.println(findNthDigit.findNthDigit(3));
     }
 
 }
