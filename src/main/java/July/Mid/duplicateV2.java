2f769fcd2ed356cf64f2cf36bfc1790505ad9514
xieli <2524546110@qq.com>
2020-07-10 19:23:03 +0800

    2020/7/10

---
+++ src/main/java/July/Mid/duplicateV2.java
@@ -0,0 +1,26 @@
+/**
+ * Created by xieli on 2020/7/10.
+ */
+public class duplicateV2 {
+    public boolean duplicate(int numbers[], int length, int[] duplication) {
+        if (numbers == null || length <= 0)
+            return false;
+
+        for (int i = 0; i < length; i++) {
+            while (numbers[i] != i) {
+                if (numbers[i] == numbers[numbers[i]]) {
+                    duplication[0] = numbers[i];
+                    return true;
+                }
+                swap(numbers, i, numbers[i]);
+            }
+        }
+        return false;
+    }
+
+    private void swap(int numbers[], int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+}
