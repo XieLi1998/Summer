5af6d7c384a5206a06d8bd3ce78fecef99d86f05
xieli <2524546110@qq.com>
2020-07-17 14:43:21 +0800

    2020/7/17

---
+++ src/main/java/July/Mid/lengthOfLongestSubstring.java
@@ -0,0 +1,10 @@
+/**
+ * Created by xieli on 2020/7/17.
+ */
+public class lengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/July/Mid/numDecodings.java
@@ -0,0 +1,41 @@
+/**
+ * Created by xieli on 2020/7/17.
+ */
+public class numDecodings {
+
+    public int numDecodings(int num) {
+        String s = String.valueOf(num);
+        int pre = 1, cur = 1;
+        //此处可以取等号
+        for (int i = 2; i <= s.length(); i++) {
+            String temp = s.substring(i - 2, i);
+            int c = (temp.compareTo("10") >= 0 && temp.compareTo("25") <= 0) ? pre + cur : cur;
+            pre = cur;
+            cur = c;
+        }
+        return cur;
+    }
+
+    public int numDecodingsV2(String s) {
+        if (s.charAt(0) == '0')
+            return 0;
+        int pre = 1, cur = 1;
+        //此处不能取等号
+        for (int i = 1; i < s.length(); i++) {
+            int temp = cur;
+            if (s.charAt(i) == '0')
+                if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2')
+                    cur = pre;
+                else
+                    return 0;
+
+            else if (s.charAt(i - 1) == '1' ||
+                    (s.charAt(i - 1) == '2' && s.charAt(i) >= '1' && s.charAt(i) <= '6'))
+                cur += pre;
+
+            pre = temp;
+        }
+        return cur;
+    }
+
+}
