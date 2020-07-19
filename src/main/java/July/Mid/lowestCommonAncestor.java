5a5240f32576fc5e29c2b7b132ccca4f4b06e281
xieli <2524546110@qq.com>
2020-07-19 22:40:08 +0800

    2020/7/19

--- src/main/java/July/Mid/lowestCommonAncestor.java
+++ src/main/java/July/Mid/lowestCommonAncestor.java
@@ -13,28 +13,28 @@ public class lowestCommonAncestor {
         }
     }
 
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        while (root != null) {
+//            if (root.val < p.val && root.val < q.val)
+//                root = root.right;
+//            else if (root.val > p.val && root.val > q.val)
+//                root = root.left;
+//            else
+//                break;
+//        }
+//        return root;
+//    }
+//
+//    public TreeNode lowestCommonAncestorV2(TreeNode root, TreeNode p, TreeNode q) {
+//
+//        if (root.val < p.val && root.val < q.val)
+//            return lowestCommonAncestor(root.right, p, q);
+//        if (root.val > p.val && root.val > q.val)
+//            return lowestCommonAncestor(root.left, p, q);
+//        return root;
+//    }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        while (root != null) {
-            if (root.val < p.val && root.val < q.val)
-                root = root.right;
-            else if (root.val > p.val && root.val > q.val)
-                root = root.left;
-            else
-                break;
-        }
-        return root;
-    }
-
-    public TreeNode lowestCommonAncestorV2(TreeNode root, TreeNode p, TreeNode q) {
-
-        if (root.val < p.val && root.val < q.val)
-            return lowestCommonAncestor(root.right, p, q);
-        if (root.val > p.val && root.val > q.val)
-            return lowestCommonAncestor(root.left, p, q);
-        return root;
-    }
-
-    public TreeNode lowestCommonAncestorV3(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q)
             return root;
 
