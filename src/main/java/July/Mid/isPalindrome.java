81b660629b66ca1159e1083894e2bbdc7374b9fd
xieli <2524546110@qq.com>
2020-07-21 23:44:56 +0800

    2020/7/21

---
+++ src/main/java/July/Mid/isPalindrome.java
@@ -0,0 +1,16 @@
+/**
+ * Created by xieli on 2020/7/21.
+ */
+public class isPalindrome {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
+}
